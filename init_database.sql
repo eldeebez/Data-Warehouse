13b76ff7bebdd7ac36db07171da56e4f55dd69c7
Abdulrhman Eldeeb <157488335+eldeebez@users.noreply.github.com>
2025-06-21 20:01:31 +0200

    Create init_database.sql

---
+++ init_database.sql
@@ -0,0 +1,18 @@
+/*
+This script purpose is to create a database for the project and the schemas that i will be staging my data through till the last stage of the project
+*/
+
+
+
+
+USE MASTER;
+CREATE DATABASE mini_project;
+GO
+USE mini_project;
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
